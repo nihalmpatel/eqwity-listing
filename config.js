94b8b6d69a51e6310c6da4ab5340df8d381795a2
Nihal Patel <hello@nihalpatel.com>
2020-10-03 18:54:56 -0400

    path config on production

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    uploadPath: 'token-logo/',
-    jsonPath: __dirname + '/main.json'
+    uploadPath: '/home/eqwitydex-frontend/token-logo/',
+    jsonPath: '/home/eqwitydex-frontend/config/main.json'
 }
--- middlewares/multer.js
+++ middlewares/multer.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./" + uploadPath);
+    cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
